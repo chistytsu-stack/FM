654b4a9d1e635d9cdcebd2a81e683c51cbd96d40
chistytsu-stack <chistytsu@gmail.com>
2025-11-08 11:08:27 +0600

    Update balance.js

--- scripts/cmds/balance.js
+++ scripts/cmds/balance.js
@@ -3,7 +3,7 @@ module.exports = {
 		name: "balance",
 		aliases: ["bal"],
 		version: "1.2",
-		author: "NTKhang",
+		author: "NTKhang (Mostakim)",
 		countDown: 5,
 		role: 0,
 		description: {
@@ -25,22 +25,38 @@ module.exports = {
 			moneyOf: "%1 đang có %2$"
 		},
 		en: {
-			money: "You have %1$",
-			moneyOf: "%1 has %2$"
+			money: "❀𝙱𝙰𝙻𝙰𝙽𝙲𝙴❀\n\n✦━━━━━━━━━━━━━━━━✦\n 𝚈𝙾𝚄𝚁 𝚁𝙴𝙲𝙴𝙽𝚃 𝙱𝙰𝙻𝙰𝙽𝙲𝙴 𝙸𝚂 %1৳\n ✦━━━━━━━━━━━━━━━━✦",
+			moneyOf: "✦━━━━━━━━━━━━━━━━✦\n %1 𝙷𝚊𝚜 %2৳\n ✦━━━━━━━━━━━━━━━━✦"
 		}
 	},
 
 	onStart: async function ({ message, usersData, event, getLang }) {
+		const formatBoldSerif = (text) => {
+			const boldSerifMap = {
+				a: "𝐚", b: "𝐛", c: "𝐜", d: "𝐝", e: "𝐞", f: "𝐟", g: "𝐠", h: "𝐡", i: "𝐢", j: "𝐣",
+				k: "𝐤", l: "𝐥", m: "𝐦", n: "𝐧", o: "𝐨", p: "𝐩", q: "𝐪", r: "𝐫", s: "𝐬", t: "𝐭",
+				u: "𝐮", v: "𝐯", w: "𝐰", x: "𝐱", y: "𝐲", z: "𝐳",
+				A: "𝐀", B: "𝐁", C: "𝐂", D: "𝐃", E: "𝐄", F: "𝐅", G: "𝐆", H: "𝐇", I: "𝐈", J: "𝐉",
+				K: "𝐊", L: "𝐋", M: "𝐌", N: "𝐍", O: "𝐎", P: "𝐏", Q: "𝐐", R: "𝐑", S: "𝐒", T: "𝐓",
+				U: "𝐔", V: "𝐕", W: "𝐖", X: "𝐗", Y: "𝐘", Z: "𝐙",
+				"0": "𝟎", "1": "𝟏", "2": "𝟐", "3": "𝟑", "4": "𝟒", "5": "𝟓", "6": "𝟔", "7": "𝟕", "8": "𝟖", "9": "𝟗",
+				"$": "$", ".": ".", ",": ",", ":": ":", "-": "-", " ": " "
+			};
+			return text.split('').map(char => boldSerifMap[char] || char).join('');
+		};
+
 		if (Object.keys(event.mentions).length > 0) {
 			const uids = Object.keys(event.mentions);
 			let msg = "";
 			for (const uid of uids) {
 				const userMoney = await usersData.get(uid, "money");
-				msg += getLang("moneyOf", event.mentions[uid].replace("@", ""), userMoney) + '\n';
+				const name = event.mentions[uid].replace("@", "");
+				msg += formatBoldSerif(getLang("moneyOf", name, userMoney)) + '\n';
 			}
-			return message.reply(msg);
+			return message.reply(msg.trim());
 		}
+
 		const userData = await usersData.get(event.senderID);
-		message.reply(getLang("money", userData.money));
+		return message.reply(formatBoldSerif(getLang("money", userData.money)));
 	}
 };
